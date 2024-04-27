3e923c74caa7076631e322b9c94c698028b784bf
𝓜𝖊𝓓𝓸𝓞 <104529732+MeDoO152@users.noreply.github.com>
2024-04-28 02:31:59 +0300

    Update start.py

--- SEMO/start.py
+++ SEMO/start.py
@@ -1022,6 +1022,40 @@ async def dev(client: Client, message: Message):
  
              
 
+     
+  @Client.on_message(filters.command(["سورس","✪ السورس ✪","السورس"], "")) 
+async def huhh(client: Client, message):
+    eply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "𝙶𝚁𝙾𝚄𝙿", url=f"https://t.me/V_l_B3"), 
+                    InlineKeyboardButton(
+                        "𝙲𝙷𝙰𝙽𝙽𝙴𝙻", url=f"https://t.me/V_l_B2"),
+                  ],[
+                    InlineKeyboardButton(
+                        "𝙼𝚎𝙳𝚘𝙾", url=f"https://t.me/V_l_B0"),
+                  ],[
+                    InlineKeyboardButton(
+                        ".💘اضف البوت اللي مجموعتك", url=f"https://t.me/{app.username}?startgroup=true"),
+                ],
+
+            ]
+
+        ),
+
+    )
+
+ await message.reply_photo(
+        photo="https://telegra.ph/file/5d5218d8cf4afd2c3e90c.jpg",
+        caption=f"""╭──── • ◈ • ────╮
+么 [᥉᥆υᖇᥴᥱ 𝙼𝚎𝙳𝚘𝙾](t.me/V_l_B2)
+么 [ժᥱ᥎ 𝙼𝚎𝙳𝚘𝙾](t.me/V_l_B0)
+么 [ ᥉υρρ᥆ᖇƚ ](t.me/V_l_B2)
+╰──── • ◈ • ────╯\n\n⍟ 𝚃𝙷𝙴 𝙱𝙴𝚂𝚃 𝚂𝙾𝚄𝚁𝙲𝙴 𝙾𝙽 𝚃𝙴𝙻𝙴𝙶𝚁𝙰𝙼""",
+        reply_markup=keyboard,
+    ) 
+
 
 
 
